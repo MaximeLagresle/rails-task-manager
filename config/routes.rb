e6e29fe26c0b0c05653b795ecb68549941023693
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-02-12 16:02:52 -0500

    As a user, I can remove a task

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # 3. Add a new task
-  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/new', to: 'tasks#new', as: :new
   post 'tasks', to: 'tasks#create'
 
   # 1. Read all the tasks
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
   # 3. Edit a task
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
   patch 'tasks/:id', to: 'tasks#update'
+
+  # 4. Delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
