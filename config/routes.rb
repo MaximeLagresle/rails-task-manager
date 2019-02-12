1294e31404591f15ff34278e280fcdbbc7c9c94b
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-02-12 15:01:08 -0500

    As a user, I can edit a task

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # 3. Add a new task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
   # 1. Read all the tasks
   get 'tasks', to: 'tasks#index'
 
   # 2. Read one specific task
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # 3. Edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
 end
